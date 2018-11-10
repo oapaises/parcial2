36d2a9f5e26b47964c089feab7c354fc3352bd16
Rafael Garcia <regestra64@gmail.com>
2018-11-10 15:41:33 -0600

    Expresiones lamda arregladas y dependecia erreglada

--- Parcial_III/src/main/java/sv/edu/uesocc/ingenieria/prn335_2018/flota/flotawebapp/boundary/jsf/backing/FrmModeloParte.java
+++ Parcial_III/src/main/java/sv/edu/uesocc/ingenieria/prn335_2018/flota/flotawebapp/boundary/jsf/backing/FrmModeloParte.java
@@ -5,10 +5,34 @@
  */
 package sv.edu.uesocc.ingenieria.prn335_2018.flota.flotawebapp.boundary.jsf.backing;
 
+import sv.edu.uesocc.ingenieria.prn335_2018.flota.datos.definicion.Modelo;
+import sv.edu.uesocc.ingenieria.prn335_2018.flota.datos.definicion.ModeloParte;
+import sv.edu.uesocc.ingenieria.prn335_2018.flota.flotawebapp.boundary.jsf.AbstractFrmDataModel;
+import sv.edu.uesocc.ingenieria.prn335_2018.flota.flotawebapp.control.AbstractFacade;
+import sv.edu.uesocc.ingenieria.prn335_2018.flota.flotawebapp.control.ModeloFacade;
+import sv.edu.uesocc.ingenieria.prn335_2018.flota.flotawebapp.control.ModeloParteFacade;
+import sv.edu.uesocc.ingenieria.prn335_2018.flota.flotawebapp.control.ParteFacade;
+
 /**
  *
  * @author christian
  */
-class FrmModeloParte {
-    
+class FrmModeloParte extends AbstractFrmDataModel<ModeloParte>{
+    public FrmModeloParte(ModeloParteFacade modeloParteFacade, ModeloFacade modeloFacade, ParteFacade parteFacade, Modelo registro ) {
+    }
+
+    @Override
+    public Object clavePorDatos(ModeloParte object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ModeloParte datosPorClave(String rowKey) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AbstractFacade<ModeloParte> getFacade() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
