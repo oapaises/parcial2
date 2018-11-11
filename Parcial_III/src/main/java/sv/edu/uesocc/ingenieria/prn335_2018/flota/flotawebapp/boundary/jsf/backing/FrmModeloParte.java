afb13f2284fbcc7a129d62f322dcdd2d25be5649
oapaises <41811599+oapaises@users.noreply.github.com>
2018-11-11 12:24:06 -0600

    tabla1 funcional

--- Parcial_III/src/main/java/sv/edu/uesocc/ingenieria/prn335_2018/flota/flotawebapp/boundary/jsf/backing/FrmModeloParte.java
+++ Parcial_III/src/main/java/sv/edu/uesocc/ingenieria/prn335_2018/flota/flotawebapp/boundary/jsf/backing/FrmModeloParte.java
@@ -6,10 +6,19 @@
 package sv.edu.uesocc.ingenieria.prn335_2018.flota.flotawebapp.boundary.jsf.backing;
 
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
+import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
+import org.primefaces.model.LazyDataModel;
 import sv.edu.uesocc.ingenieria.prn335_2018.flota.datos.definicion.Modelo;
 import sv.edu.uesocc.ingenieria.prn335_2018.flota.datos.definicion.ModeloParte;
+import sv.edu.uesocc.ingenieria.prn335_2018.flota.datos.definicion.Parte;
 import sv.edu.uesocc.ingenieria.prn335_2018.flota.flotawebapp.boundary.jsf.AbstractFrmDataModel;
 import sv.edu.uesocc.ingenieria.prn335_2018.flota.flotawebapp.control.AbstractFacade;
 import sv.edu.uesocc.ingenieria.prn335_2018.flota.flotawebapp.control.MarcaFacade;
@@ -22,10 +31,11 @@ import sv.edu.uesocc.ingenieria.prn335_2018.flota.flotawebapp.control.TipoVehicu
  *
  * @author christian
  */
-class FrmModeloParte extends AbstractFrmDataModel<ModeloParte> implements Serializable{
-    public FrmModeloParte(ModeloParteFacade modeloParteFacade, ModeloFacade modeloFacade, ParteFacade parteFacade, Modelo registro ) {
+public class FrmModeloParte extends AbstractFrmDataModel<ModeloParte> implements Serializable{
+
+    public FrmModeloParte(ModeloParteFacade modeloParteFacade, ModeloFacade modeloFacade, ParteFacade parteFacade, Modelo registro){
     }
-    
+
     @Inject
     ModeloFacade modeloFacade;
     @Inject
@@ -36,13 +46,33 @@ class FrmModeloParte extends AbstractFrmDataModel<ModeloParte> implements Serial
     ParteFacade parteFacade;
     @Inject
     ModeloParteFacade modeloParteFacade;
+    List<Parte> listaParte;
+    //public ModeloParte modePar = new ModeloParte();
+
+    @PostConstruct
+    @Override
+    public void inicializar() {
+        super.inicializar();
+        try {
+            this.listaParte = parteFacade.findAll();
+        } catch (Exception ex) {
+            this.listaParte = Collections.EMPTY_LIST;
+        }
+    }
+
+    @Override
+    public LazyDataModel<ModeloParte> getModelo() {
+        return super.getModelo();
+    }
 
     @Override
     public Object clavePorDatos(ModeloParte object) {
         if (object != null) {
             return object.getIdModeloParte();
+
         }
         return null;
+
     }
 
     @Override
@@ -55,6 +85,33 @@ class FrmModeloParte extends AbstractFrmDataModel<ModeloParte> implements Serial
             }
         }
         return null;
+
+    }
+    /**
+     * Propiedad sintetica para cambiar de Parte
+     *
+     * @return
+     */
+    public Integer getIdParteSeleccionada() {
+        if (this.registro != null && this.registro.getIdParte() != null) {
+            return this.registro.getIdParte().getIdParte();
+        }
+        return null;
+    }
+
+    /**
+     * Propiedad sintetica para cambiar de Parte
+     *
+     * @param idParte
+     */
+    public void setIdParteSeleccionada(Integer idParte) {
+        if (this.registro != null && this.listaParte != null) {
+            try {
+                this.registro.setIdParte(this.listaParte.stream().filter(m -> m.getIdParte().compareTo(idParte) == 0).collect(Collectors.toList()).get(0));
+            } catch (Exception ex) {
+
+            }
+        }
     }
 
     @Override
@@ -65,5 +122,16 @@ class FrmModeloParte extends AbstractFrmDataModel<ModeloParte> implements Serial
     @Override
     public void crearNuevo() {
         this.registro = new ModeloParte();
+
     }
+
+    public List<Parte> getListaParte() {
+        return listaParte;
+    }
+
+    public void setListaParte(List<Parte> listaParte) {
+        this.listaParte = listaParte;
+    }
+    
+    
 }
